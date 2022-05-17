915766f02ffd1b3629ba157ae9d28398f3666212
suhyeon <suhyeon5028@naver.com>
2022-05-17 16:59:54 +0900

    6. 로그아웃 처리 완료

--- src/main/java/site/metacoding/dbproject/web/UserController.java
+++ src/main/java/site/metacoding/dbproject/web/UserController.java
@@ -40,7 +40,7 @@ public class UserController {
     // 회원가입 - 로그인X
     @PostMapping("/join")
     public String join(User user) {
-        
+
         // 1. username, password, email 1.null체크, 2.공백체크
         if (user.getUsername() == null || user.getPassword() == null || user.getEmail() == null) {
             return "redirect:/joinForm";
@@ -133,6 +133,7 @@ public class UserController {
     // 로그아웃 - 로그인O
     @GetMapping("/logout")
     public String logout() {
-        return "메인페이지를 돌려주면 됨"; // PostController 만들고 수정하자.
+        session.invalidate();
+        return "redirect:/loginForm"; // PostController 만들고 수정하자.
     }
 }
